1bad73583addf313d04228205b7d177f8f19757c
Laserguner255 <138403472+Person99999999999999999999@users.noreply.github.com>
2024-07-04 11:09:58 -0400

    Added flatpak installer

--- update.sh
+++ update.sh
@@ -6,7 +6,15 @@ sudo apt upgrade -y
 sudo apt autoremove -y
 
 #Update flatpaks
-flatpak update
+if dpkg -s flatpak | grep -q 'install ok installed'; then
+    flatpak update
+else
+    sudo apt install flatpak -y
+    flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+    if ! dpkg -s gnome-software | grep -q 'install ok installed'; then
+        sudo apt install -y gnome-software-plugin-flatpak
+    fi
+fi
 
 #update snaps
 sudo snap refresh
