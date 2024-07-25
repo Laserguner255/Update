aefd843321e1f4b691d9ef69f48dc97d1bf633dc
Laserguner255 <138403472+Laserguner255@users.noreply.github.com>
2024-07-25 19:31:26 -0400

    Adds setup wizard on first run

--- update.sh
+++ update.sh
@@ -1,4 +1,61 @@
 #!/bin/bash
+PWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+#Setup
+printf 'Apt support (y/n) '
+read apt
+
+if [[ "$apt" =~ ^[Yy] ]]; then
+	echo 
+elif [[ "$apt" =~ ^[Nn] ]]; then
+	sed -i '61,63 s/^#*/#/' -i "${PWD}"/update.sh
+else
+	echo "Input not understood please try again"
+	exit
+fi
+
+printf 'Flatpak support (if it is not installed it will be if yes is selected) (y/n) '
+read flatpak
+
+if [[ "$flatpak" =~ ^[Yy] ]]; then
+	echo 
+elif [[ "$flatpak" =~ ^[Nn] ]]; then
+	sed -i '66,74 s/^#*/#/' -i "${PWD}"/update.sh
+else
+	echo "Input not understood please try again. Your previous choices have been saved"
+	exit
+fi
+
+printf 'Snap support (y/n) '
+read snap
+
+if [[ "$snap" =~ ^[Yy] ]]; then
+	echo 
+elif [[ "$snap" =~ ^[Nn] ]]; then
+	sed -i '77,77 s/^#*/#/' -i "${PWD}"/update.sh
+else
+	echo "Input not understood please try again. Your previous choices have been saved"
+	exit
+fi
+
+printf 'reboot/shutdown prompt (y/n) '
+read rest
+
+if [[ "$rest" =~ ^[Yy] ]]; then
+	sed -i '2,51 s/^#*/#/' -i "${PWD}"/update.sh 
+	trap "${PWD}"/update.sh EXIT
+	exit
+elif [[ "$rest" =~ ^[Nn] ]]; then
+	sed -i '87,98 s/^#*/#/' -i "${PWD}"/update.sh
+	sed -i '2,56 s/^#*/#/' -i "${PWD}"/update.sh
+	trap "${PWD}"/update.sh EXIT
+	exit
+else
+	echo "Input not understood please try again. Your previous choices have been saved"
+	exit
+fi
+
+#sed -i '2,51 s/^#*/#/' -i "${PWD}"/update.sh
 
 #Update apt packages
 sudo apt update
@@ -38,5 +95,6 @@ elif [[ "$answer" =~ ^[Ss] ]]; then
     sudo shutdown now
 else
     echo "Update Complete"
-    exit 0
 fi
+
+exit 
