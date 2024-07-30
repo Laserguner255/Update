13ad32d106eed0a0ad6e968b8b242f94f913691a
Laserguner255 <138403472+Laserguner255@users.noreply.github.com>
2024-07-30 08:17:58 -0400

    add devkit pro support

--- update.sh
+++ update.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 PWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-#Setup
+# Setup 
 printf 'Apt support (y/n) '
 read apt
 
 if [[ "$apt" =~ ^[Yy] ]]; then
 	echo 
 elif [[ "$apt" =~ ^[Nn] ]]; then
-	sed -i '61,63 s/^#*/#/' -i "${PWD}"/update.sh
+	sed -i '71,73 s/^#*/#/' -i "${PWD}"/update.sh
 else
 	echo "Input not understood please try again"
 	exit
@@ -20,7 +20,7 @@ read flatpak
 if [[ "$flatpak" =~ ^[Yy] ]]; then
 	echo 
 elif [[ "$flatpak" =~ ^[Nn] ]]; then
-	sed -i '66,74 s/^#*/#/' -i "${PWD}"/update.sh
+	sed -i '76,89 s/^#*/#/' -i "${PWD}"/update.sh
 else
 	echo "Input not understood please try again. Your previous choices have been saved"
 	exit
@@ -32,7 +32,19 @@ read snap
 if [[ "$snap" =~ ^[Yy] ]]; then
 	echo 
 elif [[ "$snap" =~ ^[Nn] ]]; then
-	sed -i '77,77 s/^#*/#/' -i "${PWD}"/update.sh
+	sed -i '95,95 s/^#*/#/' -i "${PWD}"/update.sh
+else
+	echo "Input not understood please try again. Your previous choices have been saved"
+	exit
+fi
+
+printf 'devkitPro support (y/n) '
+read devkitPro
+
+if [[ "$devkitPro" =~ ^[Yy] ]]; then
+	echo 
+elif [[ "$devkitPro" =~ ^[Nn] ]]; then
+	sed -i '92,92 s/^#*/#/' -i "${PWD}"/update.sh
 else
 	echo "Input not understood please try again. Your previous choices have been saved"
 	exit
@@ -42,12 +54,12 @@ printf 'reboot/shutdown prompt (y/n) '
 read rest
 
 if [[ "$rest" =~ ^[Yy] ]]; then
-	sed -i '2,51 s/^#*/#/' -i "${PWD}"/update.sh 
+	sed -i '5,68 s/^#*/#/' -i "${PWD}"/update.sh 
 	trap "${PWD}"/update.sh EXIT
 	exit
 elif [[ "$rest" =~ ^[Nn] ]]; then
-	sed -i '87,98 s/^#*/#/' -i "${PWD}"/update.sh
-	sed -i '2,56 s/^#*/#/' -i "${PWD}"/update.sh
+	sed -i '105,116 s/^#*/#/' -i "${PWD}"/update.sh
+	sed -i '5,68 s/^#*/#/' -i "${PWD}"/update.sh
 	trap "${PWD}"/update.sh EXIT
 	exit
 else
@@ -55,35 +67,41 @@ else
 	exit
 fi
 
-#sed -i '2,51 s/^#*/#/' -i "${PWD}"/update.sh
-
-#Update apt packages
+# Update apt packages 
 sudo apt update
 sudo apt upgrade -y
 sudo apt autoremove -y
 
-#Update flatpaks
+# Update flatpaks 
 if dpkg -s flatpak | grep -q 'install ok installed'; then
-    flatpak update
+	flatpak update -y
+	if ! dpkg -s gnome-software | grep -q 'install ok installed'; then
+        	sudo apt install -y gnome-software-plugin-flatpak
+        else
+        	echo ""
+        fi
 else
-    sudo apt install flatpak -y
-    flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-    if ! dpkg -s gnome-software | grep -q 'install ok installed'; then
-        sudo apt install -y gnome-software-plugin-flatpak
+	sudo apt install flatpak -y
+	flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+	if ! dpkg -s gnome-software | grep -q 'install ok installed'; then
+		sudo apt install -y gnome-software-plugin-flatpak
     fi
 fi
 
-# Update Snaps
+# devkitPro 
+sudo dkp-pacman -Syu --noconfirm
+
+# Update Snaps 
 sudo snap refresh
 
-# Clamav database
+# Clamav database 
 if dpkg -s clamav | grep -q 'install ok installed' ; then
     sudo freshclam
 else
     echo ""
 fi
 
-# Prompt for reboot
+# Prompt for reboot 
 printf 'Would you like to reboot? (r/s/n)? '
 read answer
 
